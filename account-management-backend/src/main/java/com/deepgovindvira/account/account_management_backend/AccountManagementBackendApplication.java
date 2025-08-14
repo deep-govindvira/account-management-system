ba4592e9aa98e8c15d796b15c910b542beecad11
Deep Govindvira <deepgovindvira@gmail.com>
2025-08-14 23:54:53 +0530

    chore: initial project setup

---
+++ account-management-backend/src/main/java/com/deepgovindvira/account/account_management_backend/AccountManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.deepgovindvira.account.account_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementBackendApplication.class, args);
+	}
+
+}
---
+++ account-management-backend/src/test/java/com/deepgovindvira/account/account_management_backend/AccountManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepgovindvira.account.account_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
