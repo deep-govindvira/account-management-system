f1f0a49d4cd518f01c6c323d9d0b84d95c6d1d13
Deep Govindvira <deepgovindvira@gmail.com>
2025-08-15 11:23:52 +0530

    feat: add User entity class

---
+++ account-management-backend/src/main/java/com/deepgovindvira/account/account_management_backend/entity/User.java
@@ -0,0 +1,20 @@
+package com.deepgovindvira.account.account_management_backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "app_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password; // store encrypted
+}
